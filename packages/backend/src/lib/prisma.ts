ea6af1e46bcf9ea22b17cdda5ee524cdcd9af38b
monupanwar1 <monupanwar5655@gmail.com>
2026-02-21 17:34:31 +0530

    feat: add backend package

---
+++ packages/backend/src/lib/prisma.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
